6f1f6c76fe3a8de587a0a4f2d5e9ff1c9f78f834
zakaria loulida <zakarialoulida92@gmail.com>
2024-04-03 15:47:48 +0000

    modification sur cors path

--- config/cors.php
+++ config/cors.php
@@ -14,21 +14,20 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
+    'paths' => ['user','api/*', 'sanctum/csrf-cookie'],   //we specefied our endpoints that we have 
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'],        // which type of requests are allowed ( get post are allways allowed) if i {add [put ,patch] the delete will gonna be denied  }put ,patch,delete ...
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'],  //if i put [] that mean any headers request will be accepted  i can specify which headers => [x-xsrf-token,content-type]
 
-    'exposed_headers' => [],
+    'exposed_headers' => [], //which response  headers can be access by javascript in browser
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,  //alows credentials cross origin request to come with credentials like coockies 
 
 ];
